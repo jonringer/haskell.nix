d0a15d7d844062406f46a774d6a27fa33a88383e
Jonathan Ringer <jonringer117@gmail.com>
2024-01-24 12:08:37 -0800

    Remove hls flake inputs

--- flake.nix
+++ flake.nix
@@ -23,13 +23,6 @@
       url = "git+https://gitlab.haskell.org/ghc/ghc?submodules=1";
     };
     flake-compat = { url = "github:input-output-hk/flake-compat/hkm/gitlab-fix"; flake = false; };
-    "hls-1.10" = { url = "github:haskell/haskell-language-server/1.10.0.0"; flake = false; };
-    "hls-2.0" = { url = "github:haskell/haskell-language-server/2.0.0.1"; flake = false; };
-    "hls-2.2" = { url = "github:haskell/haskell-language-server/2.2.0.0"; flake = false; };
-    "hls-2.3" = { url = "github:haskell/haskell-language-server/2.3.0.0"; flake = false; };
-    "hls-2.4" = { url = "github:haskell/haskell-language-server/2.4.0.1"; flake = false; };
-    "hls-2.5" = { url = "github:haskell/haskell-language-server/2.5.0.0"; flake = false; };
-    "hls-2.6" = { url = "github:haskell/haskell-language-server/2.6.0.0"; flake = false; };
     hackage = {
       url = "github:input-output-hk/hackage.nix";
       flake = false;
